166ef55516a28b709c1191d4e24ee1ccd26f4276
CheshireCaat <likangt2012@gmail.com>
2020-02-16 21:37:01 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const Client = require('./lib/client');
+
+const client = new Client({
+    workingDir: 'C:\\Lenovo\\BAS Remote',
+    scriptName: 'BASRemote'
+});
+
+client.onMessageReceived.addListener(message => {
+    console.log(`<--- (${message})`);
+});
+
+client.onMessageSent.addListener(message => {
+    console.log(`---> (${message})`);
+});
+
+client.start()
+    .then(() => {
+        console.log('started');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
